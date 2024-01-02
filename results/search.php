56625404b892aca4162965af6327182299221d05
dannymro71 <daniel.daian@yahoo.com>
2024-01-02 17:18:49 +0200

    version_2

--- results/search.php
+++ results/search.php
@@ -3,25 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Ajax Live Search</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Speedtest Live Search</title>
+    <link href="../src/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         .aliniere {text-align: center; vertical-align: middle}
     </style>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="../src/jquery.min.js"></script>
 </head>
 <body>
     <?php
     include_once('search_db.php');
     ?>
     <div class="container mt-4">
-        <p><h2>PHP MySQL Ajax Live Search</h2></p>
-        <h6 class="mt-5"><b>Search Name</b></h6>
+        <p><h2>Inregistrari Speedtest</h2></p>
+        <h6 class="mt-5"><b>Cauta Nume Locatie</b></h6>
         <div class="input-group mb-4 mt-3">
             <div class="form-outline">
                 <input type="text" id="getName">
             </div>
         </div>
+        <div id="" class="form-text float-start">Clic pe ID pentru vizualizare poza speedtest.</div>
         <table class=" table table-striped">
             <thead>
                 <tr class="aliniere">
@@ -51,12 +52,12 @@
             <tbody class="aliniere" id="showdata" >
                 <?php
 
-                    $sql = "SELECT * FROM speedtest_users";
+                    $sql = "SELECT * FROM speedtest_users order BY timestamp DESC";
                     $query = mysqli_query($conn, $sql);
                     while($row = mysqli_fetch_assoc($query)) {
                         ?>
                         <tr>
-                        <td><h6><?php echo $row['id'] ?></h6></td>
+                        <td><h6><a href="./?id=<?php echo $row['id'] ?>" target="_blank"><?php echo $row['id'] ?></h6></td>
                         <td><h6><?php echo $row['extra'] ?></h6></td>
                         <td><h6><?php echo $row['log'] ?></h6></td>
                         <td><h6><?php echo $row['ip'] ?></h6></td>
--- results/searchajax.php
+++ results/searchajax.php
@@ -3,11 +3,11 @@
     include_once("search_db.php");
 
     $name = $_POST['name'];
-    $sql = "SELECT * FROM speedtest_users WHERE extra LIKE '$name%'";
+    $sql = "SELECT * FROM speedtest_users WHERE extra LIKE '$name%' order by timestamp DESC";
     $query = mysqli_query($conn,$sql);
     $data = '';
     while($row = mysqli_fetch_assoc($query)) {
-        $data .= "<tr><td>" . $row['id'] . "</td>";
+        $data .= "<tr><td><a href='./?id=" . $row['id'] . "' target='_black'>" . $row['id'] . "</td>";
         $data .= "<td>" . $row['extra'] . "</td>";
         $data .= "<td>" . $row['log'] . "</td>";
         $data .= "<td>" . $row['ip'] . "</td>";
